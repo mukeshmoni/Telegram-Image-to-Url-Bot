22cdf4fb0f762ee7cfc65278f74f5b14f2eb7bbd
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2024-12-27 17:18:05 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,7 +1,4 @@
 import os
 
 class Vauth:
-  BOT_TOKEN = os.getenv("BOT_TOKEN", "8160540534:AAF8mZO0c9xkBYKsHRQavE2ap9eq8G3x-ug")# from @botfather
-    API_ID = int(os.getenv("API_ID", "28338127"))  # from https://my.telegram.org/apps
-    API_HASH = os.getenv("API_HASH", "c4cb64de16a18f8685a31716a0e2480e")  # from https://my.telegram.org/apps
-   
+  
