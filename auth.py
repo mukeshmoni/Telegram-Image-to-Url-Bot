aefaa4c2746b64a8867faa788a827c0e62d824a8
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2024-12-27 17:23:16 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,6 +2,6 @@ import os
 
 class Vauth:
   
-BOT_TOKEN = "8160540534:AAF8mZO0c9xkBYKsHRQavE2ap9eq8G3x-ug") 
+BOT_TOKEN = "8160540534:AAF8mZO0c9xkBYKsHRQavE2ap9eq8G3x-ug"
     API_ID =  "28338127"
     API_HASH =  "c4cb64de16a18f8685a31716a0e2480e"
